9f17eb8df7f1234ec27a6a4e70afc2d45a040f7c
Vovka <vovka@vovka-laptop.(none)>
2010-06-29 05:24:44 +0300

    final

--- ZendFrameworkQuickstart-20100208/features/step_definitions/calculations.rb
+++ ZendFrameworkQuickstart-20100208/features/step_definitions/calculations.rb
@@ -1,17 +1,19 @@
 # encoding: utf-8
+=begin
 Допустим /^задано первое число (\d+)$/ do |arg1|
-  @first_number = arg1.to_i
+  #@first_number = arg1.to_i
 end
 
 Допустим /^задано второе число (\d+)$/ do |arg1|
-  @second_number = arg1.to_i
+  #@second_number = arg1.to_i
 end
 
 Если /^мы попытаемся сложить два этих числа$/ do
-  @summ = @first_number + @second_number if !@first_number.nil? and !@second_number.nil?
+  #@summ = @first_number * @second_number if !@first_number.nil? and !@second_number.nil?
 end
 
 То /^должны получить (\d+)$/ do |arg1|
-  @summ.should == arg1.to_i
+  #@summ.should_not be_nil and @summ.should == arg1.to_i
 end
+=end
 
--- ZendFrameworkQuickstart-20100208/features/step_definitions/guestbook.rb
+++ ZendFrameworkQuickstart-20100208/features/step_definitions/guestbook.rb
@@ -6,11 +6,6 @@ require 'xml/libxml'
 end
 
 То /^он|^пользователь должен увидеть офигенную страницу$/ do
-#  response_body.should =~ /^<!DOCTYPE html PUBLIC "-\/\/W3C\/\/DTD XHTML 1\.0 Strict\/\/EN" "http\:\/\/www\.w3\.org\/TR\/xhtml1\/DTD\/xhtml1-strict\.dtd">/
-#  assert(
-#    (response_body =~ /<html xmlns="http\:\/\/www\.w3\.org\/1999\/xhtml">/), 
-#    response_body 
-#  );
   xhtml_validation(response_body).should be_true
 end
 
